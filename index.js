ce0bce587bba498401333ab5b15fa5b8ebff2104
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-06-14 19:09:16 -0400

    displayed image

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     fetch('http://hp-api.herokuapp.com/api/characters')
     .then(res => res.json())
-    .then(data => console.log(data)) 
+    .then(data => displayHarryPotter(data)) 
 })
+
+function displayHarryPotter(data){
+
+    let body             = document.querySelector('body')
+    let card             = document.createElement('div')
+    let name             = document.createElement('p')
+    let img              = document.createElement('img')
+
+    img.setAttribute('src', data[0].image)
+    img.setAttribute('alt', "image")
+    img.setAttribute("id", "image")
+    body.appendChild(img)
+ 
+}
 
